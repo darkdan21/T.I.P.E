2370d39336648d0e26b2e563221cfce4483a3f84
Dan Jones <dan_j@live.co.uk>
2018-12-21 22:18:22 +0000

    Add type checking decorator

--- types.py
+++ types.py
@@ -1,14 +1,24 @@
 #!/bin/python
 
+class TipeError(TypeError):
+    pass
+
+class TipeArgumentLengthError(TipeError):
+    pass
+
+class TipeArgumentTypeError(TipeError):
+    pass
+
 def tipe(*dargs):
     def real_decorator(function):
-        def wrapper(*args, **kwargs):
-            print(args)
-            print(kwargs)
-            print(dargs)
-            print(function)
-            result = function(*args, **kwargs)
-            print(result)
+        def wrapper(*args):
+            if len(args) !=  len(dargs):
+                raise TipeArgumentLengthError("Tipe decorator supplied with different number of arguments than the function")
+            for i in range(0,len(args)):
+                if type(args[i]) != dargs[i]:
+                    raise TipeArgumentTypeError("Wrong arguments supplied to function")
+
+            result = function(*args)
             return result
         return wrapper
     return real_decorator
